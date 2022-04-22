81e7a4061c71f6c839413b58c5aa0247660c34b5
MrUDDoff <kubasov.den@bk.ru>
2022-04-22 06:41:09 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/DB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class DB
+    {
+        SqlConnection connection = new SqlConnection("Data Source=COMP212-9;Initial Catalog=ТаблицаАкцийИсип20КубасовГубарев;Integrated Security=True");
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        public SqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/LoginForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DB db = new DB();
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            SqlCommand command = new SqlCommand("select * from Пользователи where логин = @UL and пароль = @P", db.getConnection());
+            command.Parameters.Add("@UL", SqlDbType.VarChar).Value = loginField.Text;
+            command.Parameters.Add("@P", SqlDbType.VarChar).Value = passField.Text;
+            adapter.SelectCommand = command;
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            if(table.Rows.Count>0)
+            {
+                if (table.Rows[0].Field<int>("idРоль") == 1)
+                {
+                    this.Hide();
+                    Form1 form1 = new Form1();
+                    form1.Show();
+                }
+                if (table.Rows[0].Field<int>("idРоль") == 2)
+                {
+                    this.Hide();
+                    Formmod formmod = new Formmod();
+                    formmod.Show();
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.Green;
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.White;
+        }
+
+        private void MainPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
